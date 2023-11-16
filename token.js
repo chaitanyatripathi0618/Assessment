2a0702a8542d640a566f8ec52904ffadc2254f79
Chaitanya Tripathi <93254346+chaitanyatripathi0618@users.noreply.github.com>
2023-11-16 23:48:26 +0530

    Update token.js

--- token.js
+++ token.js
@@ -1,11 +1,11 @@
 const { google } = require('googleapis');
 const { OAuth2Client } = require('google-auth-library');
 
-const YOUR_CLIENT_ID = '266000175007-lu957q8stbvg2r6ac24l9v53esoe3p5d.apps.googleusercontent.com';
-const YOUR_CLIENT_SECRET = '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';
-const YOUR_REDIRECT_URL = 'http://localhost:3000/oauth2callback';
+const CLIENT_ID = '266000175007-lu957q8stbvg2r6ac24l9v53esoe3p5d.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URL = 'http://localhost:3000/oauth2callback';
 
-const oauth2Client = new OAuth2Client(YOUR_CLIENT_ID, YOUR_CLIENT_SECRET, YOUR_REDIRECT_URL);
+const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
 const authUrl = oauth2Client.generateAuthUrl({
   access_type: 'offline',
